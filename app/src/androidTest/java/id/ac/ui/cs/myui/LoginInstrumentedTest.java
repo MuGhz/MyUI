ef87bf38766dac90dad46f887f6ca9a48e6398da
EKANARADHIPA DEYNAGAMA DJACARIA - 1406569806 <ekanaradhipad@gmail.com>
2017-07-14 14:46:30 +0700

    test login with username and password move activity

--- app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
@@ -14,6 +14,7 @@ import id.ac.ui.cs.myui.activity.LoginActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -46,6 +47,8 @@ public class LoginInstrumentedTest {
 
     @Test
     public void test_pindah_activity(){
+        onView(withId(R.id.username)).perform(typeText("username"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("password"),closeSoftKeyboard());
         onView(withId(R.id.login)).perform(click()).check(doesNotExist());
     }
 
