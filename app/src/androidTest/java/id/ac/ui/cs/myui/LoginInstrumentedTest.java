8c3e17c432a3d304ec4a1d84d0c9bd4bc7b937a4
EKANARADHIPA DEYNAGAMA DJACARIA - 1406569806 <ekanaradhipad@gmail.com>
2017-07-14 14:30:01 +0700

    unit test login ekawisnu

--- app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
@@ -14,6 +14,7 @@ import id.ac.ui.cs.myui.activity.LoginActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -41,4 +42,11 @@ public class LoginInstrumentedTest {
         // Type text and then press the button.
         onView(withId(R.id.login)).check(matches(withText(mButtonTextExpected)));
     }
+
+
+    @Test
+    public void test_pindah_activity(){
+        onView(withId(R.id.login)).perform(click()).check(doesNotExist());
+    }
+
 }
