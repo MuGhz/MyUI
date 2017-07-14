851a2d6854a38cd0d224e6de640eb3772680496c
Ari Kusuma <ariarcus@gmail.com>
2017-07-14 14:31:07 +0700

    unit test pindah ke home

--- app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/ui/cs/myui/LoginInstrumentedTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.myui;
 
+import android.content.Context;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,11 +10,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import id.ac.ui.cs.myui.activity.HomeActivity;
 import id.ac.ui.cs.myui.activity.LoginActivity;
 
+import static android.support.test.espresso.Espresso.getIdlingResources;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -26,14 +30,20 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class LoginInstrumentedTest {
 
     private String mButtonTextExpected;
+    private Context context;
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule(LoginActivity.class);
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mHomeRule = new ActivityTestRule(HomeActivity.class);
 
     @Before
     public void initValidString() {
         // Specify a valid string.
         mButtonTextExpected = "Login QR";
+
+        context = mActivityRule.getActivity().getApplicationContext();
     }
 
     @Test
@@ -41,4 +51,13 @@ public class LoginInstrumentedTest {
         // Type text and then press the button.
         onView(withId(R.id.login)).check(matches(withText(mButtonTextExpected)));
     }
+
+    @Test
+    public void shouldStartNextActivity() {
+        onView(withId(R.id.login))
+                .perform(click())
+                .check(doesNotExist());
+    }
+
+
 }
