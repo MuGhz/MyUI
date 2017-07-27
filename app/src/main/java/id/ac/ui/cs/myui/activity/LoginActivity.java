0f3a0feb2b480ed72bc906e70391ee642dd49988
Hafiyyan Sayyid Fadhlillah <hafiyyan94@gmail.com>
2017-07-27 16:58:14 +0700

    Merge branch 'master' of https://gitlab.cs.ui.ac.id/hafiyyan94/MyUI into ci-branch
    # Conflicts:
    #	.gitlab-ci.yml

--- app/src/main/java/id/ac/ui/cs/myui/activity/LoginActivity.java
+++ app/src/main/java/id/ac/ui/cs/myui/activity/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     private final String redirectUri = "akuncs://localhost/";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void  onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
